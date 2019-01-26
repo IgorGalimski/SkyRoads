36a61755cf12d97104c3ca2efebbf9e08cc4b18f
Игорь Галимский <galimskiy@awem.by>
2019-01-26 19:46:05 +0300

    Add logger

---
+++ Assets/Scripts/Managers/LogManager.cs
@@ -0,0 +1,27 @@
+using System;
+using System.IO;
+using UnityEngine;
+
+public class LogManager
+{
+    private const string LOG_FORMAT = "{0} [{1}] {2}";
+
+    private const string LOG_FILENAME = "log.txt";
+
+    private static string _logPath;
+    
+    [RuntimeInitializeOnLoadMethod]
+    private static void Init()
+    {
+        _logPath = Path.Combine(Application.persistentDataPath, LOG_FILENAME);
+        
+        Application.logMessageReceived += ApplicationOnLogMessageReceived;
+    }
+
+    private static void ApplicationOnLogMessageReceived(string condition, string stacktrace, LogType type)
+    {
+        Debug.Log(_logPath);
+        
+        File.AppendAllText(_logPath, string.Format(LOG_FORMAT, DateTime.Now, type, condition) + Environment.NewLine);
+    }
+}
